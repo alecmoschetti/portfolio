8e12069859e5ec93d0b38b3ca9c50a7b227ea90a
Alec Moschetti <49801141+alecmoschetti@users.noreply.github.com>
2020-10-02 15:37:25 -0700

    debugged. next up is refactor

--- js/scripts.js
+++ js/scripts.js
@@ -5,54 +5,76 @@ document.addEventListener('DOMContentLoaded', () => {
 const aboutContainer = document.querySelector('.about');
 const aboutButton = document.querySelector('.about-button');
 const header = document.querySelector('.hero');
-let bigWidthMatch = window.matchMedia('(min-width: 992px)');
-let smallWidthMatch = window.matchMedia('(max-width: 991px)');
 let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
 const quote = `${aboutContainer.innerHTML}`;
 
-const oldHero = `${header.innerHTML}`;
-
-const aboutMe = `
-<button class="close">X</button>
-    <div class="bio bio-type">
-        <h2>About Me</h2>
+const aboutMeContent = `
+        <div class="close-button-container">
+            <button class="close">X</button>
+        </div>
+        <div>
+            <h2>About Me</h2>
+        </div>
         <p>Self-taught developer, designer, and a lifelong learner.</p>
         <p>Fluent in HTML, CSS, JavaScript, jQuery, SCSS, and GIT.</p>
         <p>As long as my availability and your budget allow:</p>
         <p>no project is too big nor too small.</p>
         <p>I'm excited about the opportunity to make something for you.</p>
         <p>--Alec</p>
-    </div>
 `;
 
 //function variables
 
-const xOutSmall = (replace) => {
+const xOutSmall = () => {
     const closeButton = document.querySelector('.close');
     closeButton.addEventListener('click', function() {
-        header.innerHTML = replace;
+        const heroContainer = document.querySelector('.hero-container');
+        heroContainer.classList.remove('hidden');
+        const smallAbout = document.querySelector('.about');
+        smallAbout.classList.add('hidden');
     });
 };
 
 const xOutBig = (replace) => {
     const closeButton = document.querySelector('.close');
     closeButton.addEventListener('click', function() {
-        aboutContainer.innerHTML = replace;
+        let container = document.querySelector('.bio');
+        aboutContainer.removeChild(container);
+        const roadMap = document.createElement('div');
+        roadMap.className = 'bio';
+        roadMap.innerHTML = replace;
+        aboutContainer.appendChild(roadMap);
     });
 };
 
 const addAboutMe = () => {
-    aboutButton.addEventListener('click', (e) => {
-        aboutContainer.innerHTML = aboutMe;
+    aboutButton.addEventListener('click', () => {
+        let current = document.querySelector('.bio');
+        current.remove();
+        const aboutMe = document.createElement('div');
+        aboutMe.className = 'bio bio-type';
+        aboutMe.innerHTML = aboutMeContent;
+        aboutContainer.appendChild(aboutMe);
         xOutBig(quote);
     });
 };
 
 const smallScreenAboutMe = () => {
-    aboutButton.addEventListener('click', (e) => {
-        header.innerHTML = aboutMe;
-        xOutSmall(oldHero);
+    aboutButton.addEventListener('click', () => {
+        const navBar = document.querySelector('.nav-bar');
+        const heroContainer = document.querySelector('.hero-container');
+        heroContainer.classList.add('hidden');
+        const currentAbout = document.querySelector('.about');
+        currentAbout.remove();
+        const smallAbout = document.createElement('div');
+        smallAbout.className = 'about';
+        const smallAboutContent = document.createElement('div');
+        smallAboutContent.className = 'bio';
+        smallAboutContent.innerHTML = aboutMeContent;
+        smallAbout.appendChild(smallAboutContent);
+        header.insertBefore(smallAbout, navBar);
+        xOutSmall();
     });
 };
 
@@ -63,19 +85,10 @@ if (viewportWidth < 990) {
 }
 
 if (viewportWidth > 991) {
-	addAboutMe();
+    addAboutMe();
 }
 
-window.addEventListener("resize", function() {
-    if (bigWidthMatch.matches) {
-     addAboutMe();
-    } else if (smallWidthMatch.matches) {
-        smallScreenAboutMe();
-    }
-   });
-
-
-
+window.onresize = function(){ location.reload(); };
 
 
 }); //end domcontentloaded event handler
